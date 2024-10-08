155146d48000ae8ecc6db6cf52fd46b5a79b5729
howard-yen <47925471+howard-yen@users.noreply.github.com>
2024-10-08 11:31:14 -0400

    Update download_data.sh

--- scripts/download_data.sh
+++ scripts/download_data.sh
@@ -1,2 +1,2 @@
-wget -c https://huggingface.co/datasets/princeton-nlp/HELMET/resolve/main/data.tar.gz?download=true
+wget -c https://huggingface.co/datasets/princeton-nlp/HELMET/resolve/main/data.tar.gz
 tar -xvzf data.tar.gz
