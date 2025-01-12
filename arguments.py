810b6300c6cdd1a966675b7482777a7ac16c5b8f
Howard Yen <hyen@princeton.edu>
2025-01-12 02:17:25 -0500

    update arguments

--- arguments.py
+++ arguments.py
@@ -3,6 +3,13 @@ import yaml
 import ast
 import os
 
+import logging
+logging.basicConfig(format='%(asctime)s - %(levelname)s - %(name)s - %(message)s',
+                    datefmt='%m/%d/%Y %H:%M:%S')
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.INFO)
+
+
 def parse_arguments():
     parser = argparse.ArgumentParser(description="evaluation on downstream tasks")
     parser.add_argument("--config", type=str, default=None, help="path to config file")
@@ -12,28 +19,27 @@ def parse_arguments():
     parser.add_argument("--model_name_or_path", type=str, default=None)
     parser.add_argument("--use_vllm", action="store_true", help="whether to use vllm engine")
 
-    # data paths
-    parser.add_argument("--datasets", type=str, default=None)
-    parser.add_argument("--demo_files", type=str, default=None)
-    parser.add_argument("--test_files", type=str, default=None)
+    # data settings
+    parser.add_argument("--datasets", type=str, default=None, help="comma separated list of dataset names")
+    parser.add_argument("--demo_files", type=str, default=None, help="comma separated list of demo files")
+    parser.add_argument("--test_files", type=str, default=None, help="comma separated list of test files")
     parser.add_argument("--output_dir", type=str, default=None, help="path to save the predictions")
     parser.add_argument("--overwrite", action="store_true", help="whether to the saved file")
     parser.add_argument("--max_test_samples", type=int, default=None)
-    parser.add_argument("--num_workers", type=int, default=4)
-    parser.add_argument("--num_depths", type=int, default=10)
+    parser.add_argument("--num_workers", type=int, default=4, help="number of workers for data loading")
 
     # dataset specific settings
-    parser.add_argument("--popularity_threshold", type=int, default=3)
+    parser.add_argument("--popularity_threshold", type=int, default=3, help="popularity threshold for popqa, in log scale")
 
     # evaluation settings
-    parser.add_argument("--shots", type=int, default=5, help="total number of demos (encoder + decoder)")
+    parser.add_argument("--shots", type=int, default=2, help="total number of ICL demos")
     parser.add_argument("--input_max_length", type=str, default='8192', help="the maximum number of tokens of the input, we truncate the end of the context; can be separated by comma to match the specified datasets")
 
     # generation settings
     parser.add_argument("--do_sample", type=ast.literal_eval, choices=[True, False], default=False, help="whether to use sampling (false is greedy), overwrites temperature")
     parser.add_argument("--generation_max_length", type=str, default='10', help="max number of tokens to generate, can be separated by comma to match the specified datasets")
     parser.add_argument("--generation_min_length", type=int, default=0, help="min number of tokens to generate")
-    parser.add_argument("--temperature", type=float, default=1.0, help="generation temperature")
+    parser.add_argument("--temperature", type=float, default=0.0, help="generation temperature")
     parser.add_argument("--top_p", type=float, default=1.0, help="top-p parameter for nucleus sampling")
     parser.add_argument("--stop_newline", type=ast.literal_eval, choices=[True, False], default=False, help="whether to stop generation at newline")
 
@@ -41,7 +47,7 @@ def parse_arguments():
     parser.add_argument("--seed", type=int, default=42, help="random seed")
     parser.add_argument("--no_cuda", action="store_true", help="disable cuda")
     parser.add_argument("--no_bf16", action="store_true", help="disable bf16 and use fp32")
-    parser.add_argument("--no_torch_compile", action="store_true", help="disable cuda")
+    parser.add_argument("--no_torch_compile", action="store_true", help="disable torchcompile")
     parser.add_argument("--use_chat_template", type=ast.literal_eval, choices=[True, False], default=False, help="whether to use chat template")
     parser.add_argument("--rope_theta", type=int, default=None, help="override rope theta")
 
@@ -59,5 +65,12 @@ def parse_arguments():
 
     if args.rope_theta is not None:
         args.output_dir = args.output_dir + f"-override-rope{args.rope_theta}"
+
+    if not args.do_sample and args.temperature != 0.0:
+        args.temperature = 0.0
+        logger.info("overwriting temperature to 0.0 since do_sample is False")
+
+    if dataset == "popqa":
+        tag += f"_pop{args.popularity_threshold}"
 
     return args
